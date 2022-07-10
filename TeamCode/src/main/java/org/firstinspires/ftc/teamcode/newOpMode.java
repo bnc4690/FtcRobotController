a88b37c99dcdbdd161faa215e2fe79d518f38c01
bnc4690 <bchoi@browning.edu>
2022-07-10 18:11:14 -0400

    Add first OpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newOpMode.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevTouchSensor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+@TeleOp
+public class newOpMode extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        RevTouchSensor touch_sensor = hardwareMap.get(RevTouchSensor.class, "touch_sensor");
+        int number = 0;
+        telemetry.addData("number", number);
+        telemetry.update();
+
+        waitForStart();
+
+        while(opModeIsActive()){
+            if (touch_sensor.isPressed()){
+                telemetry.addData("Touch:", "test");
+
+            }else{
+                telemetry.addData("Sensor", "Not Pressed"); 
+            }
+            telemetry.update();
+        }
+
+    }
+}
